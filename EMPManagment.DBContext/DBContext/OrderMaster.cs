46f93ee3c439cb12518ee74d373c4a451d9d76ca
bonifatiusbvn <bhavin.bonifatius@gmail.com>
2023-12-04 17:23:22 +0530

    Pranali-Scaffolding

---
+++ EMPManagment.DBContext/DBContext/OrderMaster.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EMPManagment.API;
+
+public partial class OrderMaster
+{
+    public Guid Id { get; set; }
+
+    public string OrderId { get; set; } = null!;
+
+    public string CompanyName { get; set; } = null!;
+
+    public string Product { get; set; } = null!;
+
+    public string Quantity { get; set; } = null!;
+
+    public decimal? Amount { get; set; }
+
+    public decimal? Total { get; set; }
+
+    public DateTime OrderDate { get; set; }
+
+    public DateTime? DeliveryDate { get; set; }
+
+    public string? PaymentMethod { get; set; }
+
+    public string? DeliveryStatus { get; set; }
+
+    public DateTime? CreatedOn { get; set; }
+
+    public Guid? CreatedBy { get; set; }
+}
