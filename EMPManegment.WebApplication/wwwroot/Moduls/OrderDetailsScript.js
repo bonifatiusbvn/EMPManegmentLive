6167777e45fbcffddfbf3cc2e007f07c56b1cc37
bonifatiusbvn <bhavin.bonifatius@gmail.com>
2023-12-25 16:00:56 +0530

    Rahi-Filter In Order View

--- EMPManegment.WebApplication/wwwroot/Moduls/OrderDetailsScript.js
+++ EMPManegment.WebApplication/wwwroot/Moduls/OrderDetailsScript.js
@@ -1,10 +1,89 @@
 ﻿
-function DeliveryStatus(DeliveryStatus) {
+function SearchData() {
+    if ($('#statusform').valid()) {
+        if ($("#idStatus").val() != "All") {
+            $("#status-delivered").hide();
+            $("#status-cancelled").hide();
+            $("#status-pickups").hide();
+            $("#status-returns").hide();
+            $("#status-inprogress").hide();
+            $("#status-pending").hide();
+            $("#project-overview").hide();
+            if ($("#idStatus").val() == "Pickups") {
+                $("#deliveredactive").removeClass('active');
+                $('#pendingactive').removeClass('active');
+                $('#cancelledactive').removeClass('active');
+                $('#inprogressactive').removeClass('active');
+                $('#returnsactive').removeClass('active');
+                $('#allordersactive').removeClass('active');
+                $('#pickupactive').addClass('active');
+                var DeliveryStatus = "Pickups";
+            }
+            else {
+                if ($("#idStatus").val() == "Delivered") {
+                    $('#allordersactive').removeClass('active');
+                    $("#deliveredactive").addClass('active');
+                    $('#pendingactive').removeClass('active');
+                    $('#cancelledactive').removeClass('active');
+                    $('#inprogressactive').removeClass('active');
+                    $('#returnsactive').removeClass('active');
+                    $('#pickupactive').removeClass('active');
+                    var DeliveryStatus = "Delivered";
+                }
+                else {
+                    if ($("#idStatus").val() == "Returns") {
+                        $('#allordersactive').removeClass('active');
+                        $("#deliveredactive").removeClass('active');
+                        $('#pendingactive').removeClass('active');
+                        $('#cancelledactive').removeClass('active');
+                        $('#inprogressactive').removeClass('active');
+                        $('#returnsactive').addClass('active');
+                        $('#pickupactive').removeClass('active');
+                        var DeliveryStatus = "Returns";
+                    }
+                    else {
+                        if ($("#idStatus").val() == "Cancelled") {
+                            $('#allordersactive').removeClass('active');
+                            $("#deliveredactive").removeClass('active');
+                            $('#pendingactive').removeClass('active');
+                            $('#cancelledactive').addClass('active');
+                            $('#inprogressactive').removeClass('active');
+                            $('#returnsactive').removeClass('active');
+                            $('#pickupactive').removeClass('active');
+                            var DeliveryStatus = "Cancelled";
+                        }
+                        else {
+                            if ($("#idStatus").val() == "Pending") {
+                                $('#allordersactive').removeClass('active');
+                                $("#deliveredactive").removeClass('active');
+                                $('#pendingactive').addClass('active');
+                                $('#cancelledactive').removeClass('active');
+                                $('#inprogressactive').removeClass('active');
+                                $('#returnsactive').removeClass('active');
+                                $('#pickupactive').removeClass('active');
+                                $('#pickupactive').removeClass('active');
+                                var DeliveryStatus = "Pending";
+                            }
+                            else {
+                                if ($("#idStatus").val() == "Inprogress") {
+                                    $('#allordersactive').removeClass('active');
+                                    $("#deliveredactive").removeClass('active');
+                                    $('#pendingactive').removeClass('active');
+                                    $('#cancelledactive').removeClass('active');
+                                    $('#inprogressactive').addClass('active');
+                                    $('#returnsactive').removeClass('active');
+                                    $('#pickupactive').removeClass('active');
+                                    var DeliveryStatus = "Inprogress";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            var formData = new FormData();
+            formData.append("DeliveryStatus", DeliveryStatus);
 
-     var formData = new FormData();
-    formData.append("DeliveryStatus", DeliveryStatus);
-
-     $.ajax({
+            $.ajax({
                 url: '/OrderMaster/GetOrderDetailsByStatus',
                 type: 'Post',
                 dataType: 'json',
@@ -12,10 +91,23 @@ function DeliveryStatus(DeliveryStatus) {
                 processData: false,
                 contentType: false,
                 complete: function (Result) {
-            
-                    $("#dvdeliveredstatus").html(Result.responseText);;
-                } 
-     });       
+                    $("#dvdeliveredstatus").show();
+                    $("#dvdeliveredstatus").html(Result.responseText);
+                }
+            });
+        }
+        else {
+            window.location = '/OrderMaster/CreateOrder';
+        } 
+    }
+    else {
+        Swal.fire({
+            title: "Kindly Fill the Status",
+            icon: 'warning',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'OK',
+        })
+    }
 }
 
 
@@ -97,5 +189,46 @@ $(document).ready(function () {
     $('#createorder').on('click', function () {
         $("#createOrderForm").validate();
     });
+
+
+    $("#statusform").validate({
+        rules: {
+            idStatus: "required"
+        },
+        messages: {
+            idStatus: "Please Enter Delivered Status"            
+        }
+    })
+    $('#statussearch').on('click', function () {
+        $("#statusform").validate();
+    });      
 });
 
+$("#deliveredactive").click(function () {
+    $("#status-delivered").show();
+    $("#dvdeliveredstatus").hide();
+});
+$("#allordersactive").click(function () {
+    $("#project-overview").show();
+    $("#dvdeliveredstatus").hide();
+});
+$("#pickupactive").click(function () {
+    $("#status-pickups").show();
+    $("#dvdeliveredstatus").hide();
+});
+$("#cancelledactive").click(function () {
+    $("#status-cancelled").show();
+    $("#dvdeliveredstatus").hide();
+});
+$("#inprogressactive").click(function () {
+    $("#status-inprogress").show();
+    $("#dvdeliveredstatus").hide();
+});
+$("#pendingactive").click(function () {
+    $("#status-pending").show();
+    $("#dvdeliveredstatus").hide();
+});
+$("#returnsactive").click(function () {
+    $("#status-returns").show();
+    $("#dvdeliveredstatus").hide();
+});
