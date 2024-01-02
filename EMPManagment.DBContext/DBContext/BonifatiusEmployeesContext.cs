eff32f4ba744afa18ecb058a44f618ad5513e97b
bonifatiusbvn <bhavin.bonifatius@gmail.com>
2024-01-02 16:00:33 +0530

    Pranali - Update Table

--- EMPManagment.DBContext/DBContext/BonifatiusEmployeesContext.cs
+++ EMPManagment.DBContext/DBContext/BonifatiusEmployeesContext.cs
@@ -65,8 +65,10 @@ public partial class BonifatiusEmployeesContext : DbContext
 
     public virtual DbSet<TblVendorType> TblVendorTypes { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
 
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<OrderMaster>(entity =>
