ba43290aec99279f77df88f41c19aacf6a9f9d08
bonifatiusbvn <bhavin.bonifatius@gmail.com>
2024-02-29 12:49:32 +0530

    Pranali-Add payment method and type list

---
+++ EMPManegment.WebApplication/wwwroot/Moduls/CreditDebitMasterScript.js
@@ -0,0 +1,27 @@
+﻿
+$(document).ready(function () {
+    GetPaymentMethodList();
+    GetPaymentTypeList();
+});
+
+function GetPaymentMethodList() {
+
+    $.ajax({
+        url: '/OrderMaster/GetPaymentMethodList',
+        success: function (result) {
+            $.each(result, function (i, data) {
+                $('#txtpaymentmethod').append('<Option value=' + data.id + '>' + data.paymentMethod + '</Option>')
+            });
+        }
+    });
+}
+function GetPaymentTypeList() {
+    $.ajax({
+        url: '/ExpenseMaster/GetPaymentTypeList',
+        success: function (result) {
+            $.each(result, function (i, data) {
+                $('#paymenttype').append('<Option value=' + data.id + '>' + data.type + '</Option>')
+            });
+        }
+    });
+}
