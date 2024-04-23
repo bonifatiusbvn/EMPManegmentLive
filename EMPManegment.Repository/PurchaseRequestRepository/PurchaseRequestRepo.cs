6b1c8fef2937aefd2d1d813c09f49d38f9ad9409
bonifatiusbvn <bhavin.bonifatius@gmail.com>
2024-04-23 15:22:34 +0530

    Pranali-Create PurchaseRequest

---
+++ EMPManegment.Repository/PurchaseRequestRepository/PurchaseRequestRepo.cs
@@ -0,0 +1,19 @@
+﻿using EMPManagment.API;
+using EMPManegment.Inretface.Interface.PurchaseRequest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMPManegment.Repository.PurchaseRequestRepository
+{
+    public class PurchaseRequestRepo:IPurchaseRequest
+    {
+        public PurchaseRequestRepo(BonifatiusEmployeesContext context)
+        {
+            Context = context;
+        }
+        public BonifatiusEmployeesContext Context { get; }
+    }
+}
--- EMPManegment.WebApplication/Controllers/PurchaseRequestController.cs
+++ EMPManegment.WebApplication/Controllers/PurchaseRequestController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EMPManagment.Web.Helper;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EMPManegment.Web.Controllers
 {
     public class PurchaseRequestController : Controller
     {
+        public PurchaseRequestController(WebAPI webAPI, IWebHostEnvironment environment, APIServices aPIServices)
+        {
+            WebAPI = webAPI;
+            Environment = environment;
+            APIServices = aPIServices;
+        }
+        public WebAPI WebAPI { get; }
+        public IWebHostEnvironment Environment { get; }
+        public APIServices APIServices { get; }
+
         public IActionResult CreatePurchaseRequest()
         {
             return View();
