7889c58ccfc14df4bd087fe8c8b30a7f009bf4c3
bonifatiusbvn <bhavin.bonifatius@gmail.com>
2024-04-23 14:29:44 +0530

    Pranali-Add PRModel

---
+++ EMPManegment.EntityModels/ViewModels/Purchase Request/PurchaseRequestModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMPManegment.EntityModels.ViewModels.Purchase_Request
+{
+    public class PurchaseRequestModel
+    {
+        public Guid PrId { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public Guid ProjectId { get; set; }
+
+        public Guid? ProductId { get; set; }
+
+        public string ProductName { get; set; } = null!;
+
+        public int ProductTypeId { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        public bool? IsApproved { get; set; }
+
+        public bool? IsDeleted { get; set; }
+
+        public Guid CreatedBy { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public Guid? UpdatedBy { get; set; }
+
+        public DateTime? UpdatedOn { get; set; }
+    }
+}
