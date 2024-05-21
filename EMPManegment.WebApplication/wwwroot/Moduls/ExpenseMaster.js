5e28c825cd242c22db51c049df2e4891a728329c
bonifatiusbvn <bhavin.bonifatius@gmail.com>
2024-05-21 15:57:38 +0530

    Rahi-Solve Error

--- EMPManegment.WebApplication/wwwroot/Moduls/ExpenseMaster.js
+++ EMPManegment.WebApplication/wwwroot/Moduls/ExpenseMaster.js
@@ -862,14 +862,25 @@ $(document).ready(function () {
             },
             {
                 "render": function (data, type, full) {
-                    return '<div class="d-flex">' +
-                        '<div class="flex-grow-1 tasks_name">' + full['billNumber'] + '</div>' +
-                        '<div class="flex-shrink-0 ms-4 task-icons">' +
-                        '<ul class="list-inline tasks-list-menu mb-0">' +
-                        '<li class="list-inline-item"><a href="/ExpenseMaster/DownloadBill/?BillName=' + full.image + '"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M13 12h3l-4 4l-4-4h3V8h2v4Zm2-8H5v16h14V8h-4V4ZM3 2.992C3 2.444 3.447 2 3.999 2H16l5 5v13.993A1 1 0 0 1 20.007 22H3.993A1 1 0 0 1 3 21.008V2.992Z" /></svg></a></li>'; +
-                            '</ul>' +
-                            '</div>' +
-                            '</div>';
+                    if (full.image != null) {
+                        return '<div class="d-flex">' +
+                            '<div class="flex-grow-1 tasks_name">' + full['billNumber'] + '</div>' +
+                            '<div class="flex-shrink-0 ms-4 task-icons">' +
+                            '<ul class="list-inline tasks-list-menu mb-0">' +
+
+                            '<li class="list-inline-item"><a href="/ExpenseMaster/DownloadBill/?BillName=' + full.image + '"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M13 12h3l-4 4l-4-4h3V8h2v4Zm2-8H5v16h14V8h-4V4ZM3 2.992C3 2.444 3.447 2 3.999 2H16l5 5v13.993A1 1 0 0 1 20.007 22H3.993A1 1 0 0 1 3 21.008V2.992Z" /></svg></a></li>'; +
+                                '</ul>' +
+                                '</div>' +
+                                '</div>';
+                    } else {
+                        return '<div class="d-flex">' +
+                            '<div class="flex-grow-1 tasks_name">' + full['billNumber'] + '</div>' +
+                            '<div class="flex-shrink-0 ms-4 task-icons">' +
+                            '<ul class="list-inline tasks-list-menu mb-0">' +
+                                '</div>' +
+                                '</div>';
+                    }
+                    
                 }
             },
             {
