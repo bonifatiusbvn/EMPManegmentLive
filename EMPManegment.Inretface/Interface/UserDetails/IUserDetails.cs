909a345e28c59b66679a97fc72f601cbdf5a24d4
bonifatiusbvn <bhavin.bonifatius@gmail.com>
2024-07-04 12:40:31 +0530

    Rahi-GetEmployeeById Changes

--- EMPManegment.Inretface/Interface/UserDetails/IUserDetails.cs
+++ EMPManegment.Inretface/Interface/UserDetails/IUserDetails.cs
@@ -24,7 +24,7 @@ namespace EMPManegment.Inretface.Interface.UserList
         Task<UserResponceModel> UserLockScreen(LoginRequest UserLockScreen);
         Task<UserResponceModel> UserBirsthDayWish(Guid UserId);
         Task<IEnumerable<EmpDetailsView>> UserEdit();
-        Task<EmpDetailsView> GetById(Guid UserId);
+        Task<EmpDetailsView> GetEmployeeById(Guid UserId);
         Task<UserResponceModel> UpdateUserDetails(UserEditViewModel UpdateUser);
         Task<IEnumerable<EmpDetailsView>> GetUsersDetails();
         Task<IEnumerable<EmpDetailsView>> GetActiveDeactiveUserList();
