d94c732031d54477eeaa8e0d8d0a0ebafff94d78
Bhavin Fitter <bhavin.bonifatius@gmail.com>
2024-08-13 11:08:10 +0530

    Bhavin - Error Solve

--- EMPManegment.WebApplication/Helper/ChatHub.cs
+++ EMPManegment.WebApplication/Helper/ChatHub.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using EMPManagment.Web.Helper;
+using Microsoft.AspNetCore.SignalR;
 
 namespace EMPManegment.Web.Helper
 {
     public class ChatHub : Hub
     {
-        public ChatHub() { }
+        public ChatHub(WebAPI webAPI)
+        {
+            WebAPI = webAPI;
+        }
+
+        public WebAPI WebAPI { get; }
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
