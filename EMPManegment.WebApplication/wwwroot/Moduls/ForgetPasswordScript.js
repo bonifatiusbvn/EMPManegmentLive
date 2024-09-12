fe7994fbe5e94d5065bc8f68e2250e206baebf5a
bonifatiusbvn <bhavin.bonifatius@gmail.com>
2024-09-12 16:40:41 +0530

    Rahi-Forget Password Error Solve

--- EMPManegment.WebApplication/wwwroot/Moduls/ForgetPasswordScript.js
+++ EMPManegment.WebApplication/wwwroot/Moduls/ForgetPasswordScript.js
@@ -24,7 +24,7 @@
                     });
                 }
                 else {
-                    toastr.error(Result.message);
+                    $("#invalidmessage").text(Result.message).show();
                 }
             },
         })
