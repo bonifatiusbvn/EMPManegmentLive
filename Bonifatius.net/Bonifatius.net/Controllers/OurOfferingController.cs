2c56f2e02659fa2d26ec188c49bcfc4a1db3bff7
Bhavin Fitter <bhavin.bonifatius@gmail.com>
2024-10-10 10:31:47 +0530

    Bhavin - New Project Add

---
+++ Bonifatius.net/Bonifatius.net/Controllers/OurOfferingController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bonifatius.net.Controllers
+{
+    public class OurOfferingController : Controller
+    {
+        public IActionResult TurnkeySolutions()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessAutomation()
+        {
+            return View();
+        }
+
+        public IActionResult EngineeringSolutions()
+        {
+            return View();
+        }
+        public IActionResult SoftwareDevelopment()
+        {
+            return View();
+        }
+        public IActionResult IOTServices()
+        {
+            return View();
+        }
+
+        public IActionResult LidarMapping()
+        {
+            return View();
+        }
+
+        public IActionResult ProductManufacturing()
+        {
+            return View();
+        }
+
+        public IActionResult EquipmentManufacturing()
+        {
+            return View();
+        }
+    }
+}
