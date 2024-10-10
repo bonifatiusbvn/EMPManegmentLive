71ef1630ca07d62d23fc752f468b2a4feba8c402
Bhavin Fitter <bhavin.bonifatius@gmail.com>
2024-10-10 10:43:08 +0530

    Bhavin - Error Solve

--- Bonifatius.net/Bonifatius.net/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Bonifatius.net.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
