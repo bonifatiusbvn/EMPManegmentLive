19fc830b204ce407f6d7cce08d69a2bd6a8a7e1f
bonifatiusbvn <bhavin.bonifatius@gmail.com>
2025-07-01 15:03:46 +0530

    Pranali-Add message notification in header layout

--- EMPManegment.WebApplication/wwwroot/Moduls/ChatMessagesScript.js
+++ EMPManegment.WebApplication/wwwroot/Moduls/ChatMessagesScript.js
@@ -225,7 +225,7 @@ function GetAllNotifications() {
         url: '/Home/GetUserAllNotifications',
         type: 'GET',
         dataType: 'html',
-        complete: function (Result) {
+        complete: function (Result) {debugger
             $('#userAllNotificationId').html(Result.responseText);
             var totalcount = $('#totalNotificationCount').val();
             $('#CountAllNotification').text(totalcount);
@@ -236,7 +236,9 @@ function GetAllNotifications() {
                 $('#CountunreadNotification').text(totalcount);
             }
             var UnreadMessages = $('#TotalUnreadMessages').val();
+            debugger
             $("#CountUnreadMessage").text(UnreadMessages);
+            $("#CountunreadMessageNotification").text(UnreadMessages);
             var totalTasks = $('#totalTotalTasks').val();
             $("#CountTotalTask").text(totalTasks);
         },
